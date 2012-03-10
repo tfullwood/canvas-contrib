3cd6a71cf1a0516db2b3a94d8d627d2d50532f12
Kevin Hansen <kajigga@gmail.com>
2012-03-09 17:02:24 -0700

    Generalizing the domain, token, and course_id

--- API_Examples/PullCourseGrades/pull_grades.py
+++ API_Examples/PullCourseGrades/pull_grades.py
@@ -3,21 +3,22 @@ import json
 
 # API reference https://canvas.instructure.com/doc/api/submissions.html
 
-domain='cwt'
-BASE_URL = "https://%s.instructure.com/api/v1%s"
-access_token = "h3xyPlREWirj4FDjJTzlpAWPakHgVzCx0nJlwYA4XcX2tvqa6j6YEBo7AoOfvSQW"
-course_id = 242844  # not the sis_id but the canvas internal id
+BASE_URL = "https://<your_domain>.instructure.com/api/v1%s"
+access_token = "<your_access_token>"
+course_id = <the_course_id>  # not the sis_id but the canvas internal id
 
 REQUEST_HEADERS = {'Authorization':'Bearer %s' % access_token}
 
 # First, get the list of students in the course
 
-students_endpoint = BASE_URL % (domain,'/courses/%d/students' % (course_id))
+students_endpoint = BASE_URL % '/courses/%d/students' % (course_id)
 
 # Create a request, adding the REQUEST_HEADERS to it for authentication
 student_request = urllib2.Request(students_endpoint,None,REQUEST_HEADERS)
+
 # Fetch the response, read it and strip extra whitespace from the beginning and end
 student_response = urllib2.urlopen(student_request).read().strip()
+
 # Load the response as JSON
 response_data = json.loads(student_response)
 
@@ -31,7 +32,7 @@ student_ids = [s['id'] for s in response_data]
 
 
 # Build the endpoint for requesting submissions
-submissions_endpoint = BASE_URL % (domain,'/courses/%d/students/submissions' % (course_id))
+submissions_endpoint = BASE_URL % '/courses/%d/students/submissions' % (course_id)
 
 # Build the GET request parameters that are needed to fetch the submissions along with the
 # total scores (grades)
@@ -47,5 +48,4 @@ req = urllib2.Request(submissions_endpoint + "?" +submission_params,None,{'Autho
 grades_response = urllib2.urlopen(req).read().strip()
 
 # Load the response as JSON
-grades = json.loads(submissions_response)
-
+grades = json.loads(grades_response)
