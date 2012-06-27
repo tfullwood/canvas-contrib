ba4ddb776fe546a0eac9850449316955f1f3779a
Kevin Hansen <kajigga@gmail.com>
2012-06-27 14:05:09 -0600

    tweaked "disable what if grades" script and added a README.md for it.

---
+++ Branding/Customizations/disable_whatif/README.md
@@ -0,0 +1,9 @@
+Disabling the What-If grades and comparative scores
+======
+
+This is an unsupported, community-created project. Keep that in 
+mind. Instructure won't be able to help you fix or debug this.
+That said, the community will hopefully help support and keep
+both the script and this documentation up-to-date.
+
+Good luck!
--- Branding/Customizations/disable_whatif/disable_whatif.js
+++ Branding/Customizations/disable_whatif/disable_whatif.js
@@ -1,3 +1,11 @@
+/*
+ * This is an unsupported, community-created project. Keep that in 
+ * mind. Instructure won't be able to help you fix or debug this.
+ * That said, the community will hopefully help support and keep
+ * both the script and this documentation up-to-date.
+ * 
+ * Good luck!
+ */
 $(document).ready(function(){
  if(window.location.pathname.search('grades')>0){
     $('td.assignment_score').unbind();
@@ -9,6 +17,6 @@ $(document).ready(function(){
       }
       return null;
     });
-
+    $('a.toggle_comments_link.tooltip').hide();
  }
 });
