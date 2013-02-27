bc80ecb3c110b6fa6f566aa6c0dca3ab1aff6f85
Kevin Hansen <kajigga@gmail.com>
2013-02-27 10:05:38 -0700

    Added several misc javascript files. Added necessary folders for the bash_curl SIS integration script.

---
+++ Branding/Customizations/javascript/README.rst
@@ -0,0 +1,16 @@
+Information about the scripts in this folder
+============================================
+
+
+pr_gallery_
+
+When a user is viewing an assignment submission, the file does not automatically
+preview for images (maybe not other filetypes either).  This javascript makes uploaded
+images render below the "File Uploads" box.
+
+Disclaimer: This script is distributed with no guarantee, warrantee, or any other type
+of '-antee'.  No support is provided either.  You use at your own judgement and need to understand
+that your are responsible for it working or not working.
+
+
+.. _pr_gallery link: peer_review_as_gallery.js
---
+++ Branding/Customizations/javascript/peer_review_as_gallery.js
@@ -0,0 +1,51 @@
+/*
+ * README.
+ * When a user is viewing an assignment submission, the file does not automatically
+ * preview for images (maybe not other filetypes either).  This javascript makes uploaded
+ * images render below the "File Uploads" box.
+ *
+ * Disclaimer: This script is distributed with no guarantee, warrantee, or any other type
+ * of '-antee'.  No support is provided either.  You use at your own judgement and need to understand
+ * that your are responsible for it working or not working.
+ */
+
+var pr = {
+  init : function(){
+    var pat = /courses\/[\d]*\/assignments\/[\d]*\/submissions\/[\d]*/;
+    if(pat.test(window.location.href)){
+        var iframe = window.frames[1];
+        // console.log($(iframe).width());
+        var content = $(iframe.document.getElementById('content'));
+        var a = content.find('a.comment_attachment_link.image'); 
+        var img = new Image();
+        img.src = a.attr('href');
+
+        var content_wrapper = iframe.document.getElementById('content-wrapper');
+        $(img).insertAfter(content_wrapper);
+        img.onload = function(){
+          // console.log(img.width);
+          if(img.width>$(iframe).width()){
+            $(img).attr('width', '100%');
+          }
+        };
+      }
+  }
+};
+
+
+$(document).ready(pr.init);
+
+
+var b1;
+for(x=0;x<$('b').length;x++){ 
+  if($($('b')[x]).text()=='Assigned Peer Reviews'){ 
+    b1=$('b')[x];
+    break;
+  }
+}
+
+if(b1!===undefined){
+  var l = $(b1.nextElementSibling);
+  var reviews = l.find('a:visible');
+  // Count the reviews, save the list to a cookie for use later.
+}
