ffb100c8f3eb0c691207ad691376601fe158a27f
Kevin Hansen <kajigga@gmail.com>
2013-03-08 14:56:28 -0700

    Added Java Example

---
+++ SIS Integration/java/basic_examples/CanvasAPI/config.js
@@ -0,0 +1,6 @@
+{ access_token:"",
+account_id:"",
+domain:"cwt.test",
+import_file:"/path/to/import_file.zip",
+filetype:"zip"
+}
---
+++ SIS Integration/java/basic_examples/src/com/coolwebteacher/canvasapi/APIConfig.java
@@ -0,0 +1,12 @@
+package com.coolwebteacher.canvasapi;
+
+public class APIConfig {
+
+	public String access_token;
+	public String domain;
+	public String account_id;
+	public String import_file;
+	public String filetype = "zip";
+	public String logfile;
+
+}
