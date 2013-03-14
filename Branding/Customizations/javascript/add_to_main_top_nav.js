ed2a6830dbed75f6aa0efdd686c75e7fafa0b844
Kevin Hansen <kajigga@gmail.com>
2013-03-14 14:24:37 -0600

    Javascript to add a menu item to the main big top nav (not next to help)

---
+++ Branding/Customizations/javascript/add_to_main_top_nav.js
@@ -0,0 +1,3 @@
+var url = "https://google.com";
+var label = "Google";
+$('#menu').append("<li class='menu-item'> <a href='" + url + "' class='menu-item-no-drop' target='_blank'>"+label+"</a> </li>");
