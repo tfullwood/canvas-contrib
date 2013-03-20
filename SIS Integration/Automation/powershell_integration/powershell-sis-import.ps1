75b625142a39aa590e586a052e36833793fc4fa6
Kevin Hansen <kajigga@gmail.com>
2013-03-20 09:27:02 -0600

    Adjusted Powershell SIS import script

--- SIS Integration/Automation/powershell_integration/powershell-sis-import.ps1
+++ SIS Integration/Automation/powershell_integration/powershell-sis-import.ps1
@@ -1,11 +1,14 @@
 $sourceDir = ".\" #this is source directory literal path
 $outputPath = ".\" #output path for the zip file creation
 $outputZip = ".\courses1.csv.zip" # name of the zip file
-$url = "https://cwt.test.instructure.com/api/v1/accounts/82726/sis_imports.json?import_type=instructure_csv&access_token="
-$token = "h3xyPlREWirj4FDjJTzlpAWPakHgVzCx0nJlwYA4XcX2tvqa6j6YEBo7AoOfvSQW" # access_token
+$account_id = "82726"
+$url = "https://<your_domain>.instructure.com/api/v1/accounts/"+$account_id+"/sis_imports.json?import_type=instructure_csv"
+$token = "<access_token_here>" # access_token
+$headers = @{"Authorization"="Bearer "+$token}
+
 $contentType = "application/zip" # don't change
 $InFile = $outputPath+$outputZip # don't change
-$uri = $url+$token # don't change
+$uri = $url 
 
 
 #write-zip -Path $sourceDir"*.csv" -OutputPath $outputPath$outputZip
@@ -17,9 +20,6 @@ $results = invoke-RestMethod -InFile $InFile -Method POST -PassThru -ContentType
 Write-Host "output here"
 
 Write-Host $results.workflow_state
-
-
-
 
 Move-Item $outputPath$outputZip $outputPath$t-$outputZip
 Remove-Item $sourceDir*.csv
