e8f43f63477c8355c2a94f5d5f3a670f6616933c
Kevin Hansen <kajigga@gmail.com>
2013-06-27 09:41:48 -0600

    Added script that will convert files to UTF-8 (without a BOM) using powershell.

---
+++ SIS Integration/powershell_integration/convertToUTF8.ps1
@@ -0,0 +1,6 @@
+﻿foreach($i in ls -name "C:\Users\kevinh\Desktop\courses1_csv\*.csv")
+{
+    $file = get-content "C:\Users\kevinh\Desktop\courses1_csv\$i"
+    $encoding = New-Object System.Text.UTF8Encoding($False)
+    [System.IO.File]::WriteAllLines("C:\Users\kevinh\Desktop\courses2_csv\" + $i, $file, $encoding)
+}
