95b380511dda1865d2330e05d585b0cabfb5bfe5
Ian Morris <ianm@instructure.com>
2014-04-21 13:08:25 -0600

    Added consideration for mobile browsers and the self enrollment screen.

--- Branding/Customizations/change_login_username/change_login_username.js
+++ Branding/Customizations/change_login_username/change_login_username.js
@@ -1,17 +1,29 @@
 /**
- * JQuery is the javascript library used in many places across canvas.  
+ * JQuery is the javascript library used in many places across canvas.
  *
  * This script describes how you would change the text in the email box on the Canvas
- * Login page.  By default, the text in the box is "Email" but this script will allow you
- * to change this.  
+ * Login page and Self Enrollment screen.  By default, the text in the box is "Email"
+ * but this script will allow you to change this.
  *
- * When implementing this script, replace <replacement> with the text you want to appear.
- * For example, you may want to have that text be "A-Number" rather than "email".  
- *
- *
+ * When implementing this script, replace <REPLACEMENT> with the text you want to appear.
+ * For example, you may want to have that text be "A-Number" rather than "email".
  *
  **/
-$(document).ready(function(){
-  var sp = $('#login_form label[for=pseudonym_session_unique_id]>span');
-  sp.text('<replacement>');
+
+
+$(document).ready(function() {
+    // login screen
+    if (window.location.pathname.search('login')) {
+        if ($('#f1_container').length == 0) { // not a mobile browser
+            var sp = $('#login_form label[for=pseudonym_session_unique_id]>span');
+            sp.text('<REPLACEMENT>');
+        } else { // mobile browser
+            $('input[name="pseudonym_session[unique_id]"]').attr("placeholder", "<REPLACEMENT>");
+        }
+    }
+    // self enrollment screen
+    if (window.location.pathname.search('enroll')) {
+        $('label[for="student_email"]').text("<REPLACEMENT>");
+        $('p:contains("Please enter your email and password")').text("Please enter your <REPLACEMENT> and password");
+    }
 });
