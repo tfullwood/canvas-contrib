b4d9c60a6bfea8f671d573605a1234dd4cf03942
MattAttack7 <matt.superman@gmail.com>
2015-06-16 21:34:07 -0600

    add folder structure for JavaScript and README files

--- Branding/JavaScript/course_navigation_image_footer.js
+++
@@ -1,4 +0,0 @@
-///////////////////////////////
-//Add course navigation link //
-///////////////////////////////
-$('#left-side').append('<div id="canvas_brand"><img src="https://dl.dropbox.com/s/sritd562f6hmgst/CanvasLogo.png"></div>');
